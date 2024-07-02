980311a2898f30922b52e5f80b8668071e15f8c0
Genius30 <geniusporwal01@gmail.com>
2024-07-02 16:14:41 +0530

    Import bug fix

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -26,4 +26,4 @@ const playlistSchema = new Schema(
   }
 );
 
-export const PlayList = mongoose.model("Playlist", playlistSchema);
+export const Playlist = mongoose.model("Playlist", playlistSchema);
